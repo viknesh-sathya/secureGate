1a49ccbc33792363d40cf7b061c30023f8357cba
viknesh-sathya <iamviknesh.sathya@gmail.com>
2026-03-31 02:35:03 +0530

    updated the mailer.js file

--- backend/mails/mailer.js
+++ backend/mails/mailer.js
@@ -1,29 +1,33 @@
 import SibApiV3Sdk from "sib-api-v3-sdk";
 
 const client = SibApiV3Sdk.ApiClient.instance;
-
-// Configure API key authorization
 client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
 
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-export const sendEmail = async (to, subject, html) => {
+export const sendEmail = async ({ to, subject, html }) => {
   try {
     const emailData = {
       sender: {
         name: "SecureGate",
-        email: "no-reply@securegate.com", // You can use your Brevo sender email
+        email: process.env.BREVO_MAIL,
       },
       to: [{ email: to }],
       subject,
       htmlContent: html,
     };
 
+    console.log("Sending email with:", emailData);
+
+    // Use the apiInstance here
     const response = await apiInstance.sendTransacEmail(emailData);
-    console.log("📧 Email sent successfully:", response.messageId);
+    console.log("📧 Email sent successfully:", response);
     return true;
   } catch (error) {
-    console.error("❌ Email sending failed:", error.message);
+    console.error(
+      "❌ Email sending failed:",
+      error.response?.text || error.message,
+    );
     return false;
   }
 };
